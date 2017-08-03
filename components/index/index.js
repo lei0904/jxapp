f1a2da524213575f2d69810e42771d0ce2139bb7
Jerry Ou <xioudi@gmail.com>
2017-08-03 12:50:01 +0800

    [update] active

--- components/index/index.js
+++ components/index/index.js
@@ -12,6 +12,7 @@ module.exports = {
     template: __inline('index.ftl'),
     data: function () {
         return {
+            loaded: false,
             banners: [],
             combos: []
         }
@@ -40,6 +41,44 @@ module.exports = {
         toComboDetail: function (combo) {
             new Storage('session').set('ComboDetail', Storage.stringify(combo));
             this.$router.push({ path: '/comboDetail' });
+        },
+
+        sendAppActive: function () {
+            var s = new Storage('local');
+            var str = s.get('_app_active_');
+            var data = null;
+            if (str) {
+                data = Storage.parse(str);
+            }
+
+            var today = new Date();
+            var today_date = today.getFullYear() + '' + (today.getMonth() + 1) + '' + today.getDate();
+
+            if (data && data.date) {
+                var date = data.date;
+                if (date === today_date) {
+                    return;
+                }
+            }
+
+            var _this = this;
+            var cfg = require.config().config;
+            var pkg = null;
+            if (cfg && cfg.pkg) {
+                pkg = cfg.pkg;
+            }
+            if (!pkg) {
+                return;
+            }
+
+            _this.$api.get('api/app/active', {pkg : pkg}).then(function (rets) {
+                _this.$api.process(rets, function (rets) {
+                    console.log(rets);
+                    s.set('_app_active_', Storage.stringify({
+                        date: today_date
+                    }))
+                });
+            });
         }
     },
     computed: {
@@ -51,14 +90,23 @@ module.exports = {
             })
         }
     },
-    mounted:function(){
-        var _this = this;
-        _this.$api.get('api/main', {}).then(function (rets) {
-            _this.banners = rets.data.banners;
-            _this.combos = rets.data.combos;
-            console.log(_this.combos);
-        }).catch(function (e) {
-            Cui.MessageBox.alert('数据加载失败！', '系统提示');
-        });
+    activated: function () {
+        if (!this.loaded) {
+            var _this = this;
+            _this.$api.get('api/main', {}).then(function (rets) {
+                _this.banners = rets.data.banners;
+                _this.combos = rets.data.combos;
+
+                _this.loaded = true;
+
+                setTimeout(function () {
+                    _this.sendAppActive();
+                }, 2000);
+
+
+            }).catch(function (e) {
+                Cui.MessageBox.alert('数据加载失败！', '系统提示');
+            });
+        }
     }
 };
