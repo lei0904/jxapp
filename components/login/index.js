ddeed493af89617874da1142f4aaa5edc540c182
Jerry Ou <xioudi@gmail.com>
2017-08-11 17:19:05 +0800

    add

--- components/login/index.js
+++ components/login/index.js
@@ -19,32 +19,54 @@ module.exports = {
                     _this.$validator.renderError();
                     return;
                 }
+                if (Ces.Config.plugin) {
+                    Ces.JSBridge.callHandler('jpush', [], function (rets) {
+                        if (rets && rets.status === 1 && rets.data) {
+                            var pushId = rets.data;
+                            _this.$api.post('api/login', {
+                                mobile: _this.user,
+                                pushId: pushId,
+                                password: _this.password
+                            }).then(function (rets) {
+                                if (rets.status !== 'OK') {
+                                    Cui.Toast({
+                                        message: rets.message,
+                                        position: 'bottom'
+                                    });
+                                } else {
+                                    _this.$api.LoginData.set(rets.data, function () {
+                                        _this.$router.push({ path: '/index' });
+                                    }, function () {
+                                        Cui.MessageBox.alert('系统异常，登录失败');
+                                    });
+                                }
+                            });
+                        } else {
+                            Cui.MessageBox.alert('系统异常，登录失败');
+                        }
+                    });
+                } else {
+                    var pushId = '1104a897929c9173620';
+                    _this.$api.post('api/login', {
+                        mobile: _this.user,
+                        pushId: pushId,
+                        password: _this.password
+                    }).then(function (rets) {
+                        if (rets.status !== 'OK') {
+                            Cui.Toast({
+                                message: rets.message,
+                                position: 'bottom'
+                            });
+                        } else {
+                            _this.$api.LoginData.set(rets.data, function () {
+                                _this.$router.push({ path: '/index' });
+                            }, function () {
+                                Cui.MessageBox.alert('系统异常，登录失败');
+                            });
+                        }
+                    });
+                }
 
-                Ces.JSBridge.callHandler('jpush', [], function (rets) {
-                    if (rets && rets.status === 1 && rets.data) {
-                        var pushId = rets.data;
-                        _this.$api.post('api/login', {
-                            mobile: _this.user,
-                            pushId: pushId,
-                            password: _this.password
-                        }).then(function (rets) {
-                            if (rets.status !== 'OK') {
-                                Cui.Toast({
-                                    message: rets.message,
-                                    position: 'bottom'
-                                });
-                            } else {
-                                _this.$api.LoginData.set(rets.data, function () {
-                                    _this.$router.push({ path: '/index' });
-                                }, function () {
-                                    Cui.MessageBox.alert('系统异常，登录失败');
-                                });
-                            }
-                        });
-                    } else {
-                        Cui.MessageBox.alert('系统异常，登录失败');
-                    }
-                });
             });
         },
         register: function () {
--- components/shuttleList/index.ftl
+++ components/shuttleList/index.ftl
@@ -3,7 +3,11 @@
         <!--<div class="part_header">我的接送</div>-->
         <div v-show="!empty" class="part_content">
             <div class="list_item clearfix" v-for="item in list">
-                <div> {{item.shuttle_date}} {{item.shuttle_time}} </div>
+                <div> {{item.shuttle_date}} {{item.shuttle_time}}
+                    <span class="s_status" v-if="item.audit_status == 1">待审核</span>
+                    <span class="s_status" v-if="item.audit_status == 2">审核通过</span>
+                    <span class="s_status" v-if="item.audit_status == 3">审核驳回</span>
+                </div>
                 <div> {{item.shuttle_address}} </div>
             </div>
         </div>
